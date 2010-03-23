dfbdc8f0aacc8912a7066bd443385d5d791f5670
Paul Carduner <paul@carduner.net>
2010-03-23 14:18:59 -0700

    More documentation.

---
+++ README.rst
@@ -0,0 +1,85 @@
+=============
+jQuery Motion
+=============
+
+The jQuery motion plugin makes it easy to animate arbitrarily complex
+dom manipulation.
+
+For example, let's say you want to move several list items from one
+unordered list to another - maybe for adding products to a shopping cart.
+You might write a javascript function with jQuery that looks like
+this::
+
+  function addItemsToShoppingCart(){
+    $("#items li.selected").appendTo("#shopping-cart");
+  }
+
+Which gets executed when a user pushes a button::
+
+  $("#add-to-cart").click(addItemsToShoppingCart);
+
+With the jQuery motion plugin, you can make the selected items move
+gracefully into the shopping cart without having to change your
+existing dom manipulation code.  Using motion it would look like
+this::
+
+  $("#add-to-cart").click(
+    function(){
+      $("#items li,#shopping-cart li").motion(addItemsToShoppingCart);
+    });
+
+Or in a more concise form::
+
+  $("#add-to-cart").click($("#items li,#shopping-cart li").withMotion(addItemsToShoppingCart));
+
+
+In Depth Overview
+-----------------
+
+Motion works by taking a snapshot of the DOM before and after it has
+been manipulated, keeping track of the positions of all the DOM
+elements being moved.  Using fixed positioning, the DOM elements are
+then "moved" from their old location to their new location on the
+page.
+
+Provided Functions
+..................
+
+- **motion** - Animate the selected elements using the given DOM
+    manipulation function.
+
+  - *arguments:*
+
+    - **mover** - A function that performs some DOM manipulation.
+
+    - **speed** - The speed of the animation.  This can be any value
+      accepted by jQuery.animate. (i.e. "slow", "medium", "fast")
+
+    - **callback** - A function to call once all animations have
+        completed.
+
+    - **spacerClass** - A css class to be added to spacer elements.
+        Defaults to ``__spacer__``.  Spacer elements are used to
+        maintain the size and shape of the original location of an
+        animated dom element while the animation is taking place.
+
+    - **maxEffected** - The maximum number of elements that should be
+        animated.  If more than this number of elements are selected,
+        then animations will be turned off.  Animating too many
+        elements can slow down the browser.  Defaults to 100.
+
+    These arguments can be passed in sequentially like so::
+
+      $(".effected").motion(mover, speed, callback, spacerClass, maxEffected);
+
+    or as a JavaScript object like so::
+
+      $(".effected").motion({mover:someFunc,
+                             speed:"slow",
+                             maxEffected:50});
+
+- **withMotion** - Return a function for use as event handlers that
+    just call the ``motion`` function.  This takes all the same
+    arguments at the ``motion`` function.  For example::
+
+      $("#some-button").click($(".effected").withMotion(someMoverFunc));
--- jquery.motion.js
+++ jquery.motion.js
@@ -1,9 +1,17 @@
 (function($){
-     $.fn.motion = function(mover, callback, spacerClass, maxAffected){
+     $.fn.withMotion = function(mover, speed, callback, spacerClass, maxEffected){
+         var that = this;
+         return function(){
+             $(that).motion(mover, speed, callback, spacerClass, maxEffected);
+         };
+     };
+
+     $.fn.motion = function(mover, speed, callback, spacerClass, maxEffected){
          var config = {
              spacerClass: "__spacer__",
              callback: $.noop || function(){},
-             maxAffected: 100
+             maxEffected: 100,
+             speed: "medium"
          };
          if ($.isFunction(mover)){
              $.extend(
@@ -12,19 +20,19 @@
                      mover: mover,
                      callback: callback,
                      spacerClass: spacerClass,
-                     maxAffected: maxAffected
+                     maxEffected: maxEffected
                  });
          } else {
              $.extend(config,mover);
          }
-         var affected = this;
-         if (affected.length > config.maxAffected){
+         var effected = this;
+         if (effected.length > config.maxEffected){
              config.mover.call(this);
              config.callback();
              return;
          }
-         // step 1: go through affected elements and save their current position.
-         affected.each(
+         // step 1: go through effected elements and save their current position.
+         effected.each(
              function(){
                  $.data(this, "fromPosition", $(this).offset());
              });
@@ -35,7 +43,7 @@
          var spacers = [];
          function check(){
              if (animating === 0){
-                 affected.css({position:'', left:'', top:''});
+                 effected.css({position:'', left:'', top:''});
                  for (var i=0,ii=spacers.length; i<ii; i++){
                      spacers[i].remove();
                  }
@@ -44,9 +52,9 @@
          }
          var docViewTop = $(window).scrollTop();
          var docViewBottom = docViewTop + $(window).height();
-         affected.each(
+         effected.each(
              function(){
-                 // save new position of affected element
+                 // save new position of effected element
                  var $this = $(this);
                  var toPosition = $this.offset();
                  var fromPosition = $.data(this, "fromPosition");
@@ -55,7 +63,7 @@
                      return;
                  }
                  /**
-                  * animate the affected element.  This will automatically skip over elements which:
+                  * animate the effected element.  This will automatically skip over elements which:
                   *  a) have not actually moved
                   *  b) are not within the viewable window space.
                   */
@@ -64,7 +72,7 @@
                       (toPosition.top < docViewBottom && toPosition.top >= docViewTop))){
 
 
-                     // add spacers for affected element
+                     // add spacers for effected element
                      var spacer = $('<'+this.tagName+' class="'+this.className+' '+config.spacerClass+'">'
                                     + '</'+this.tagName+'>')
                          .css(
@@ -83,7 +91,7 @@
                              top:toPosition.top+"px",
                              left:toPosition.left+"px"
                          },
-                         "slow",
+                         config.speed,
                          function(){
                              animating--;
                              check();
